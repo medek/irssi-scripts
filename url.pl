99088abcaf685bacedff28076698ec6ba0f074e6
Gavin Massey <mdk@mystacktrace.org>
2013-06-09 02:08:02 +0100

    Made url process multiple arguments so things like /url <id> <id> <id> will open all avaliable urls

--- url.pl
+++ url.pl
@@ -36,12 +36,12 @@ sub url_command
 	my ($data, $server, $witem) = @_;
 	my @args = split(/ /, $data);
 	my $browser = Irssi::settings_get_str('url_browser');
-	my $url = $urls[int $args[0]];
-	if($url eq '')
+	my $url = '';
+	for my $i (0..(scalar @args)-1)
 	{
-		return;
+		$url = $urls[int $args[$i]];
+		Irssi::command("exec - $browser \"$url\" > /dev/null 2> /dev/null");
 	}
-	Irssi::command("exec - $browser \"$url\" > /dev/null 2> /dev/null");
 }
 
 sub privmsg_sig
